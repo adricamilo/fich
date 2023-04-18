0220f7d6e1334317a10c39d8894e325a4ec799c7
adricamilo <adrian.vasconez@gmail.com>
2023-04-18 02:12:17 -0500

    pdf_processing.py now uses pymupdf library, tested

--- pdf_processing.py
+++ pdf_processing.py
@@ -1,27 +1,32 @@
 import os
-import tempfile
-import pdf2image
+import fitz
 import fich
-import gc
 
 pdf_folder = os.path.join("fich_database", "pdfs")
-file_set = set()
+file_set = list()
 
 for dir_, _, files in os.walk(pdf_folder):
     for file_name in files:
         file = os.path.join(pdf_folder, file_name)
         if os.path.isfile(file) and file.endswith(".pdf"):
-            file_set.add(file)
+            file_set.append(file)
+
+file_set.sort()
+
+print(len(file_set))
 
 image_folder = os.path.join("fich_database", "images")
 
-counter = 0
+page_num = 0
+file_num = 1
 
 for file in file_set:
-    with tempfile.TemporaryDirectory() as path:
-        images = pdf2image.convert_from_path(file, output_folder=path)
-        for image in images:
-            # noinspection PyProtectedMember
-            image.save(fich._get_name(counter, "page", ".png"))
-            counter = counter + 1
-    print(file, gc.collect())
+    print(f"Starting file {file_num}/{len(file_set)}...", file)
+    with fitz.open(file) as pdf:
+        for page in pdf:
+            pix = page.get_pixmap()  # render page to an image
+            name = os.path.join(image_folder, fich._get_name(page_num, "page", ".png"))
+            pix.save(name)  # store image as a PNG
+            page_num = page_num + 1
+    print(f"Completed file {file_num}/{len(file_set)}")
+    file_num = file_num + 1
